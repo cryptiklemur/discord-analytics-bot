d663363cc627023b3fe87d4748a97558d1b28a54
Aaron Scherer <aequasi@gmail.com>
2017-07-02 12:37:47 -0700

    Fixing bot

--- src/Aggregator.js
+++ src/Aggregator.js
@@ -15,22 +15,6 @@ export default class Aggregator {
         
         console.log(`Aggregating Message Receive Events for ${kernel.client.guilds.size} guilds`);
         // Delete user banned, user left, and user join
-        console.log("Deleting banned events");
-        UserBannedEvent.remove({}, err => {
-            if (err) console.error(err);
-        });
-        console.log("Deleting left events");
-        UserLeftEvent.remove({}, err => {
-            if (err) console.error(err);
-        });
-        console.log("Deleting joined events");
-        UserJoinedEvent.remove({}, err => {
-            if (err) console.error(err);
-        });
-        console.log("Deleting voice events");
-        VoiceEvent.remove({}, err => {
-            if (err) console.error(err);
-        });
         
         try {
             await Aggregator.aggregateMessagesReceived(minsAgo);
